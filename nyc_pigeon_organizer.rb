cb1a5df0a03f10f24453983cf0631df95c748835
Ali Madni <ali555m@hotmail.com>
2019-02-14 12:23:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  by_name = {} #final hash to be returned
+
+  data.each do |attribute, values|#color, gender, live and there corresponding values
+    values.each do |value, pigeons|#individual values and there corresponding pigeons
+      pigeons.each do |pigeon|#individual pigeons
+        if !(by_name[pigeon])#if the new hash does not contain hash for a particular pigeon, create one
+          by_name[pigeon] = {}
+        end
+        if !(by_name[pigeon][attribute]) #if the hash for each pigeon does contain an array for a particular attribute, create one
+          by_name[pigeon][attribute] = []
+        end
+        by_name[pigeon][attribute] << value.to_s
+      end
+    end
+  end
+  by_name
 end
